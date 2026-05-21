c1102cfe229ace9aaabe90afcb32d89567e85036
Arnel-rah <arnel.raharinandrasana@student.passerellesnumeriques.org>
2026-05-21 15:37:03 +0300

    test: add endpoint tests for HelloController

--- src/main/java/school/hei/demo/endpoint/hello/HelloController.java
+++ src/main/java/school/hei/demo/endpoint/hello/HelloController.java
@@ -1,4 +1,4 @@
-package school.hei.demo.endpoint.rest.controller.hello;
+package school.hei.demo.endpoint.hello;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
